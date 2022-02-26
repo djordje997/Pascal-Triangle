01a16a254dcffee539cb23168ca4903467368c47
djordje997 <djordjesarvan97@gmail.com>
2022-02-26 17:19:40 +0100

    changes fixed

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,41 +2,38 @@ class PascalTriangle
   
   def initialize(number_of_rows)
     @number_of_rows = number_of_rows
-    @triangle = []
+    @triangle = nil
   end
 
   def to_a
-    if @triangle == []
-      @triangle = create_triangle
-    end
-   @triangle
+    @tringle || create_triangle
   end
 
   def to_s
     triangle = to_a
     string_triangle = ""
-    last_row = triangle[-1].join(" ")
-    longest_row = last_row.length
+    longest_row_length = triangle.last.join(" ").length
     triangle.each do |current|
-      current_lenght = current.join(" ").length
-      (longest_row / 2 - current_lenght / 2).times {string_triangle += " "}
-      string_triangle += current.join(" ")
+      current_row_formated = current.join(" ")
+      current_length = current_row_formated.length
+      spacing = (longest_row_length - current_length) / 2
+      string_triangle << " " * spacing
+      string_triangle += current_row_formated
       string_triangle += "\n"
     end
-    p string_triangle.inspect
     string_triangle
   end
 
   private
 
     def create_triangle
-    triangle = []
+    @triangle = []
     row = [1]
     @number_of_rows.times do
-      triangle << row
+      @triangle << row
       row = generate_next_row(row)
     end
-    triangle
+    @triangle
     end
   
     def generate_next_row(row)
