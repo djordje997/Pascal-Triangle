d324df9d98881bce12f92959fc79a45cba986df3
djordje997 <86196338+djordje997@users.noreply.github.com>
2022-03-07 16:27:16 +0100

    two identations fixed

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -21,30 +21,30 @@ class PascalTriangle
 
   private
 
-    def formated_triangle_row(current, longest_row_length)
-      current_row_formated = current.join(" ")
-      spacing = (longest_row_length - current_row_formated.length) / 2
-      formated_triangle = " " * spacing
-      formated_triangle += current_row_formated
-      formated_triangle += "\n"
-      formated_triangle
-    end
+  def formated_triangle_row(current, longest_row_length)
+    current_row_formated = current.join(" ")
+    spacing = (longest_row_length - current_row_formated.length) / 2
+    formated_triangle = " " * spacing
+    formated_triangle += current_row_formated
+    formated_triangle += "\n"
+    formated_triangle
+  end
 
-    def triangle
-      triangle = []
-      @triangle ||= create_triangle(triangle)
-    end
+  def triangle
+    triangle = []
+    @triangle ||= create_triangle(triangle)
+  end
 
-    def create_triangle(triangle)
+  def create_triangle(triangle)
     row = [1]
     @number_of_rows.times do
       triangle << row
       row = generate_next_row(row)
     end
     triangle
-    end
+  end
   
-    def generate_next_row(row)
-      ([0] + row).zip(row + [0]).collect { |a, b| a + b }
-    end
+  def generate_next_row(row)
+    ([0] + row).zip(row + [0]).collect { |a, b| a + b }
+  end
 end
